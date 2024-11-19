7552085d838e2be22cc0105beb5ad5010c08f4a6
Felix Krueger <96026278+KFlix9@users.noreply.github.com>
2024-11-19 23:48:52 +0100

    genaral: major refactoring / code fomatting, updated: character count with sort

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     character_count_dict = get_character_count(text)
-    xx = character_count_dict_to_sorted_list(character_count_dict)
+    character_count_single_sorted = character_count_dict_to_sorted_list(character_count_dict)
     # ouput
     print(f"### Begin of the book report on {book_path} ###")
     print(f"{get_word_count(text)} words found in the document")
     print()
 
-    key = character_count_dict_to_sorted_list(character_count)
-    # for key in character_count.keys():
-    #     if key.isalpha():
-    #         print(f"the '{key}' was found {character_count[key]} times")
-    print(f"\n### End of the book report on {book_path} ###")
+    for item in character_count_single_sorted:
+        if item['char'].isalpha():
+            print(f"the '{item['char']}' was found {item['num']} times")
 
+    print()
+    print(f"### End of the book report on {book_path} ###")
 
 def get_text(book_path):
     with open(book_path) as f:
@@ -52,6 +52,9 @@ def character_count_dict_to_sorted_list(character_count):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list 
 
+
+def sort_on(d):
+    return d["num"]
 
 if __name__ == "__main__":
     main()
